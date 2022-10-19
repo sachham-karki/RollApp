a4dd31be2fddf6cd9050c28fb8a8e42adae3b5a6
SachKar <88773679+sachham-karki@users.noreply.github.com>
2022-10-20 08:44:10 +1100

    Removed comment participant

--- client/src/pages/Participant.jsx
+++ client/src/pages/Participant.jsx
@@ -3,19 +3,6 @@ import React from "react";
 import axios from "axios";
 
 export default function Participant() {
-  // const [data, setData] = useState({ items: "", items: "" });
-
-  // const handleInput = (event) => {
-  //   let newInput = { [event.target.name]: event.target.value };
-  //   setData({ ...data, ...newInput });
-  // };
-
-  // const deleteFirstDataInSpinner = async () => {
-  //   await axios
-  //     .delete("http://localhost:8000/spinner")
-  //     .then(() => console.log("sucess"));
-  //   console.log("fuck you");
-  // };
   return (
     <form action="/spinner" method="POST">
       <input type="text" placeholder="Items" name="Items" />
