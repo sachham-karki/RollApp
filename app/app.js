cfe0ed328b15222560d92d2afbf4ebf90a487e41
Luqman Tuke <59395383+luqmantuke@users.noreply.github.com>
2022-10-27 11:50:25 +0300

    Fixing PORT issue for heroku env

--- app/app.js
+++ app/app.js
@@ -70,7 +70,7 @@ io.sockets.on("connection", (socket) => {
 const connectDB = require("./db/connect");
 
 //Defining the port.
-const port = 8000 || process.env.port;
+const port = process.env.PORT || 80;
 
 const start = async () => {
   try {
